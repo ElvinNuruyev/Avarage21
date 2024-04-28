15a70c16e47c3c6544cb5a1b3642f5bcc3cb666d
User <rootelvin@gmail.com>
2024-04-28 16:15:00 +0400

    Birinci Deyisiklis edildi

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        var n = 43;
+        var m = 21;
+        var j = 0;
+        var cem = 0;
+        var avr = 1;
+        for (int i=m; i<= n; i++)
+        {
+            if (i % 21 == 0)
+            {
+                j++;
+                cem += i;
+                avr = cem / j;
+            }
+        }
+
+        
+        Console.WriteLine(avr);
+    }
+}
